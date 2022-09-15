730fcbab0954e3a4905be7b18a26bfb2613d5815
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-09-15 12:29:06 -0500

    add additional event listeners

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 /* Imports */
 
 /* Get DOM Elements */
-/*const cityInput = document.getElementById('cityname');
-const climateSelect = document.getElementById('climate');*/
+const cityInput = document.getElementById('cityname');
+const climateSelect = document.getElementById('climate');
 const buildSelect = document.getElementById('build');
-/*const dispCity = document.getElementById('inputcity');
-const dispClimate = document.getElementById('selclimate');*/
+const dispCity = document.getElementById('inputcity');
+const dispClimate = document.getElementById('selclimate');
 const dispBuild = document.getElementById('selbuild');
 
 /* State */
 const ad = {
-    buildish: 'skyscraper',
+    city: '',
+    buildish: '',
+    climate: '',
 };
 
 /* Events */
+cityInput.addEventListener('input', () => {
+    ad.city = cityInput.value;
+    displayAd();
+});
+
 buildSelect.addEventListener('change', () => {
     ad.buildish = buildSelect.value;
+    displayAd();
+});
+
+climateSelect.addEventListener('change', () => {
+    ad.climate = climateSelect.value;
     displayAd();
 });
 
@@ -25,7 +37,9 @@ function dispControl() {
     buildSelect.value = ad.buildish;
 }
 function displayAd() {
-    dispBuild.src = 'assets/buildish/' + ad.buildish + '.png';
+    dispCity.textContent = ad.city;
+    dispBuild.src = 'assets/buildish/' + ad.buildish + '.jpg';
+    dispClimate.src = 'assets/climate/' + ad.climate + '.jpg';
 }
 // (don't forget to call any display functions you want to run on page load!)
 
