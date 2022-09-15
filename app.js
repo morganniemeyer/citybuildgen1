0a91c350313f2f89873b0c5b2f5243a5ec859eea
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-09-15 12:09:52 -0500

    begin functions for page update

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 /* Imports */
 
 /* Get DOM Elements */
-const cityInput = document.getElementById('cityname');
-const climateSelect = document.getElementById('climate');
+/*const cityInput = document.getElementById('cityname');
+const climateSelect = document.getElementById('climate');*/
 const buildSelect = document.getElementById('build');
+/*const dispCity = document.getElementById('inputcity');
+const dispClimate = document.getElementById('selclimate');*/
+const dispBuild = document.getElementById('selbuild');
 
 /* State */
+const ad = {
+    buildish: 'skyscraper',
+};
 
 /* Events */
+buildSelect.addEventListener('change', () => {
+    ad.buildish = buildSelect.value;
+    displayAd();
+});
 
 /* Display Functions */
 
+function dispControl() {
+    buildSelect.value = ad.buildish;
+}
+function displayAd() {
+    dispBuild.src = 'assets/buildish/' + ad.buildish + '.png';
+}
 // (don't forget to call any display functions you want to run on page load!)
+
+dispControl();
+displayAd();
